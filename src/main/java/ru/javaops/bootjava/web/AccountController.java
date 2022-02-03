0737fc67cfa89a85322517ad5bff0a9858f0de49
SkipperProg <ambeloslydcev@yandex.ru>
2022-02-03 22:11:21 +0300

    4_0_4_auth_DB

---
+++ src/main/java/ru/javaops/bootjava/web/AccountController.java
@@ -0,0 +1,19 @@
+package ru.javaops.bootjava.web;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.javaops.bootjava.AuthUser;
+import ru.javaops.bootjava.model.User;
+
+@RestController
+@RequestMapping(value = "/api/account")
+public class AccountController {
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public User get(@AuthenticationPrincipal AuthUser authUser) {
+        return authUser.getUser();
+    }
+}
