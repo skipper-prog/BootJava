38a51fb0d430f088a2586030994f637ec0df1968
SkipperProg <ambeloslydcev@yandex.ru>
2022-03-12 12:32:37 +0300

    4_0_4_auth_DB

--- src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
+++ src/main/java/ru/javaops/bootjava/RestaurantVotingApplication.java
@@ -1,27 +1,14 @@
 package ru.javaops.bootjava;
 
 import lombok.AllArgsConstructor;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.javaops.bootjava.repository.UserRepository;
 
 @SpringBootApplication
-//@AllArgsConstructor
-public class RestaurantVotingApplication implements ApplicationRunner {
-    private final UserRepository userRepository;
+@AllArgsConstructor
+public class RestaurantVotingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantVotingApplication.class, args);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) {
-        System.out.println(userRepository.findByLastNameContainingIgnoreCase("last"));
-    }
-
-    public RestaurantVotingApplication(UserRepository userRepository){
-        this.userRepository = userRepository;
     }
 }
